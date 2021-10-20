7c421158d994bc62d5727af8cf81ed0897b8b669
Jovan Bienvenu <jbienvenu@wisersoftware.com>
2021-10-20 19:02:42 +0200

    Add socketio webRTC

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ app.use(cors());
 app.use(router);
 
 io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name });
+    });
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal);
+    });
+
     socket.on("join", ({ name, room }, callback) => {
         const { user, error } = addUser({ id: socket.id, name, room });
         if (error) return callback(error);
@@ -44,6 +54,7 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", () => {
         const user = removeUser(socket.id);
+        socket.broadcast.emit("callEnded");
 
         if (user) {
             io.to(user.room).emit("message", { user: "admin", text: `Oh non! ${user.name} est parti :(` });
